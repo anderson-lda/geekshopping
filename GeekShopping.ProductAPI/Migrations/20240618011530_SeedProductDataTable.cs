4341f3d843e506a75da3f3cd4de907f95b41cbf5
anderson-lda <anderson.lima.araujo@hotmail.com>
2024-06-17 22:27:45 -0300

    crud finished

---
+++ GeekShopping.ProductAPI/Migrations/20240618011530_SeedProductDataTable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 3L, "Category", "Integer tempus vulputate felis, id eleifend urna vestibulum sit amet. Pellentesque ligula libero, tincidunt in rutrum a, condimentum eget velit. Vestibulum semper mattis iaculis. Fusce molestie nibh eget ipsum porta, vitae iaculis magna efficitur. Nullam quis mi sit amet erat fermentum tincidunt vel id dolor. Nulla sit amet lacinia ipsum.", "https://github.com/anderson-lda/geekshopping/blob/main/ShoppingImages/3_vader.jpg?raw=true", "Name 3", 69.9m },
+                    { 4L, "Category", "Integer tempus vulputate felis, id eleifend urna vestibulum sit amet. Pellentesque ligula libero, tincidunt in rutrum a, condimentum eget velit. Vestibulum semper mattis iaculis. Fusce molestie nibh eget ipsum porta, vitae iaculis magna efficitur. Nullam quis mi sit amet erat fermentum tincidunt vel id dolor. Nulla sit amet lacinia ipsum.", "https://github.com/anderson-lda/geekshopping/blob/main/ShoppingImages/11_mars.jpg?raw=true", "Name 4", 69.9m },
+                    { 5L, "Category", "Integer tempus vulputate felis, id eleifend urna vestibulum sit amet. Pellentesque ligula libero, tincidunt in rutrum a, condimentum eget velit. Vestibulum semper mattis iaculis. Fusce molestie nibh eget ipsum porta, vitae iaculis magna efficitur. Nullam quis mi sit amet erat fermentum tincidunt vel id dolor. Nulla sit amet lacinia ipsum.", "https://github.com/anderson-lda/geekshopping/blob/main/ShoppingImages/10_milennium_falcon.jpg?raw=true", "Name 5", 69.9m },
+                    { 6L, "Category", "Integer tempus vulputate felis, id eleifend urna vestibulum sit amet. Pellentesque ligula libero, tincidunt in rutrum a, condimentum eget velit. Vestibulum semper mattis iaculis. Fusce molestie nibh eget ipsum porta, vitae iaculis magna efficitur. Nullam quis mi sit amet erat fermentum tincidunt vel id dolor. Nulla sit amet lacinia ipsum.", "https://github.com/anderson-lda/geekshopping/blob/main/ShoppingImages/11_mars.jpg?raw=true", "Name 6", 69.9m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 6L);
+        }
+    }
+}
--- GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -8,5 +8,45 @@ namespace GeekShopping.ProductAPI.Model.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) {}
 
         public DbSet<Product> Products { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 3,
+                Name = "Name 3",
+                Price = new decimal(69.9),
+                Description = "Integer tempus vulputate felis, id eleifend urna vestibulum sit amet. Pellentesque ligula libero, tincidunt in rutrum a, condimentum eget velit. Vestibulum semper mattis iaculis. Fusce molestie nibh eget ipsum porta, vitae iaculis magna efficitur. Nullam quis mi sit amet erat fermentum tincidunt vel id dolor. Nulla sit amet lacinia ipsum.",
+                ImageURL = "https://github.com/anderson-lda/geekshopping/blob/main/ShoppingImages/3_vader.jpg?raw=true",
+                CategoryName = "Category",
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 4,
+                Name = "Name 4",
+                Price = new decimal(69.9),
+                Description = "Integer tempus vulputate felis, id eleifend urna vestibulum sit amet. Pellentesque ligula libero, tincidunt in rutrum a, condimentum eget velit. Vestibulum semper mattis iaculis. Fusce molestie nibh eget ipsum porta, vitae iaculis magna efficitur. Nullam quis mi sit amet erat fermentum tincidunt vel id dolor. Nulla sit amet lacinia ipsum.",
+                ImageURL = "https://github.com/anderson-lda/geekshopping/blob/main/ShoppingImages/11_mars.jpg?raw=true",
+                CategoryName = "Category",
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 5,
+                Name = "Name 5",
+                Price = new decimal(69.9),
+                Description = "Integer tempus vulputate felis, id eleifend urna vestibulum sit amet. Pellentesque ligula libero, tincidunt in rutrum a, condimentum eget velit. Vestibulum semper mattis iaculis. Fusce molestie nibh eget ipsum porta, vitae iaculis magna efficitur. Nullam quis mi sit amet erat fermentum tincidunt vel id dolor. Nulla sit amet lacinia ipsum.",
+                ImageURL = "https://github.com/anderson-lda/geekshopping/blob/main/ShoppingImages/10_milennium_falcon.jpg?raw=true",
+                CategoryName = "Category",
+            });
+            modelBuilder.Entity<Product>().HasData(new Product
+            {
+                Id = 6,
+                Name = "Name 6",
+                Price = new decimal(69.9),
+                Description = "Integer tempus vulputate felis, id eleifend urna vestibulum sit amet. Pellentesque ligula libero, tincidunt in rutrum a, condimentum eget velit. Vestibulum semper mattis iaculis. Fusce molestie nibh eget ipsum porta, vitae iaculis magna efficitur. Nullam quis mi sit amet erat fermentum tincidunt vel id dolor. Nulla sit amet lacinia ipsum.",
+                ImageURL = "https://github.com/anderson-lda/geekshopping/blob/main/ShoppingImages/11_mars.jpg?raw=true",
+                CategoryName = "Category",
+            });
+        }
     }
 }
