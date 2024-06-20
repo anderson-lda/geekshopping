30405f3446ebcdedc2e2f024daff4eaf68a95017
anderson-lda <anderson.lima.araujo@hotmail.com>
2024-06-19 23:43:49 -0300

    created all the pages

--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -16,8 +16,58 @@ namespace GeekShopping.Web.Controllers
 
         public async Task<IActionResult> ProductIndex()
         {
-            var products = _productService.FindAllProducts();
+            var products = await _productService.FindAllProducts();
             return View(products);
+        }
+        
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var reponse = await _productService.CreateProduct(model);
+                if(reponse != null) return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductUpdate(int id)
+        {
+            var model = await _productService.FindProductById(id);
+            if(model != null) return View(model);
+            else return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var reponse = await _productService.UpdateProduct(model);
+                if (reponse != null) return RedirectToAction(nameof(ProductIndex));
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> ProductDelete(int id)
+        {
+            var model = await _productService.FindProductById(id);
+            if (model != null) return View(model);
+            else return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel model)
+        {
+            var reponse = await _productService.DeleteProductById(model.Id);
+            if (reponse) return RedirectToAction(nameof(ProductIndex));
+
+            return View(model);
         }
     }
 }
